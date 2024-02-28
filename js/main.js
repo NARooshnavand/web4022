afc1a6994a8d35feb4c051d10dcef26c62f3844f
NARooshnavand <nasserakhoundi@gmail.com>
2024-02-28 04:05:45 -0800

    change position of an element

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ console.log("this is the first code in javascript")
 var  mdisplay = true;
 function myFunction(){
     const element = document.getElementById("ptag");
+    const mybox = document.getElementById("mybox");
     // console.log(element);
     if(mdisplay)
     {
@@ -11,5 +12,7 @@ function myFunction(){
         element.style.display = "block";
         mdisplay = true;
     }
+    mybox.style.left = "300px";
+
     alert("You clicked on link!")
 }
